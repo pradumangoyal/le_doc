82c9d16345f5dcdaf349b971c8149aaf368be45c
pradumangoyal <goyal.praduman611@gmail.com>
2018-12-04 14:00:32 +0530

    Add ML equivalent code framework view in 'brain_tumour'

--- brain_tumour/urls.py
+++ brain_tumour/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from brain_tumour import views
 
 urlpatterns = [
-    path('list/', views.BrainTumourList.as_view())
+    path('list/', views.BrainTumourList.as_view()),
+    path('operate/', views.operate)
 ]
