d084ec2124a0576ca0887b5875f10c16ed9614ae
Praduman Goyal <pgoyal@ma.iitr.ac.in>
2018-12-10 11:09:17 +0530

    Change file structure to zip for `brain_stroke`

--- brain_stroke/views.py
+++ brain_stroke/views.py
@@ -9,7 +9,31 @@ from doc_doc.settings import MEDIA_ROOT
 from patients.models import Patient
 from brain_stroke.serializers import BrainStrokeSerializer
 from brain_stroke.models import BrainStroke
+
+import zipfile
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+import numpy as np
+import cv2
 # Create your views here.
+
+#For power transform in images
+def adjust_gamma(image, gamma=1.0):
+	invGamma = 1.0 / gamma
+	table = np.array([((i / 255.0) ** invGamma) * 255
+		for i in np.arange(0, 256)]).astype("uint8")
+ 
+	return cv2.LUT(image, table)
+
+#For finding the min value of passed array
+def min_index(array):
+    min=0
+    for i in range(1,len(array)):
+        if array[i]<array[min]:
+            min=i
+    return min 
 
 class BrainStrokeList(generics.ListCreateAPIView):
     queryset = BrainStroke.objects.all()
@@ -50,20 +74,33 @@ def operate(request):
         if request.user == obj.patient.doctor:
             if not obj.clustered:
                 #It will give you an image opened in PIL
-                img=Image.open(obj.uploaded.file.name).convert('L') 
+                # img=Image.open(obj.uploaded.file.name).convert('L') 
                 intermediate_path = MEDIA_ROOT+'/brain_stroke/'+obj.patient.username+'/orignal/intermediate_'+os.path.basename(obj.uploaded.file.name)
+
                 #It will give you an image opened in PIL
-                
+                with zipfile.ZipFile(obj.uploaded.file.name, 'r') as zip_ref:
+                    zip_ref.extractall(intermediate_path)
+
+                item = []
+                files = os.listdir(intermediate_path)
+                print(files)
+                intermediate_root_dir = intermediate_path+'/'+files[0]
+                print(files[0])
+                print(intermediate_root_dir)
+                item = os.listdir(intermediate_root_dir)
+                item=[intermediate_root_dir + s for s in item]
+                print(item)
+
                 #Equivalent to ML part converting in B&W
-                bw = img.point(lambda x: 0 if x<128 else 255, '1')
-                obj.stage = "3" #storing predicted value
-                bw.save(intermediate_path)    
+                # bw = img.point(lambda x: 0 if x<128 else 255, '1')
+                # obj.stage = "3" #storing predicted value
+                # bw.save(intermediate_path)    
                 #Equivalent to ML part converting in B&W
 
                 #Save the image as clustered
-                file = open(intermediate_path, 'rb')
-                obj.clustered.save(os.path.basename(obj.uploaded.file.name), File(file))
-                os.remove(intermediate_path)
+                # file = open(intermediate_path, 'rb')
+                # obj.clustered.save(os.path.basename(obj.uploaded.file.name), File(file))
+                # os.remove(intermediate_path)
                 #Save the image as clustered
 
             return Response(data=BrainStrokeSerializer(obj).data)
